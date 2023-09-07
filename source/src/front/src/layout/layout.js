70ac9ac6c23d6a208470a0b789ab72a3c523f10a
mediny <mediny0613@gmail.com>
2023-09-07 09:42:17 +0900

    no message

--- source/src/front/src/layout/layout.js
+++ source/src/front/src/layout/layout.js
@@ -1,6 +1,7 @@
 
 import Footer from "../layout/js/footer";
 import Header from "../layout/js/header";
+import './css/Header.css';
 const Layout=()=>{
     return(
         <div>
@@ -10,7 +11,7 @@ const Layout=()=>{
 
             </main>
 
-            <Footer/>
+            {/*<Footer/>*/}
         </div>
     )
 }
