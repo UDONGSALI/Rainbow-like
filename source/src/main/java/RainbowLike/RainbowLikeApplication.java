110d4a4e2e3c4fb5c666bd0d2b5ad1c33b09118f
e22222yn <e22222yn@gmail.com>
2023-09-07 11:43:49 +0900

    Revert "게시판 번호별 조회하는 방법 clubController에서 getClub 참고해주세요~ setBoardNumber 안의 숫자만 해당하는 게시판 번호로 바꿔주시면 됩니다. Long 타입이니까 L 붙여주시구요."
    This reverts commit 4facbff649507f5d1f60e7273d24815165c9acad.

--- source/src/main/java/RainbowLike/RainbowLikeApplication.java
+++ source/src/main/java/RainbowLike/RainbowLikeApplication.java
@@ -8,24 +8,20 @@ import RainbowLike.repository.BoardRepository;
 import RainbowLike.repository.MemberRepository;
 import RainbowLike.repository.PostRepository;
 import RainbowLike.service.MemberService;
-
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Arrays;
 
-
 @SpringBootApplication
 @RequiredArgsConstructor
 public class RainbowLikeApplication implements CommandLineRunner {
-
 
     private final MemberService memberService;
 
@@ -36,7 +32,6 @@ public class RainbowLikeApplication implements CommandLineRunner {
 	private MemberRepository memberRepository;
         @Autowired
         private PostRepository postRepository;
-
     public static void main(String[] args) {
         SpringApplication.run(RainbowLikeApplication.class, args);
 
@@ -55,9 +50,9 @@ public class RainbowLikeApplication implements CommandLineRunner {
 		memberRepository.saveAll(Arrays.asList(member1, member2));
 
 		Post club1 = new Post(member1, board1, "소모임게시글테스트", "소모임게시글테스트용입니다. 겸사겸사 게시판과 멤버도 테스트 중입니다.", LocalDateTime.now(), 0, "허가", "모집중");
-        Post club2 = new Post(member1, board1, "학원 시작하고 엄마를 한 번도 못 봤어요", "엄마보고싶어요", LocalDateTime.now(), 0, "허가", "진행중");
-        Post test1 = new Post(member2, board2, "엄마는 나 안 보고 싶을 수도 있어", "사실 그럴 가능성이 더 큰 편이죠", LocalDateTime.now(), 0, "거부", "거부");
-		postRepository.saveAll(Arrays.asList(club1, club2, test1));
+        Post club2 = new Post(member1, board2, "학원 시작하고 엄마를 한 번도 못 봤어요", "엄마보고싶어요", LocalDateTime.now(), 0, "허가", "진행중");
+        Post club3 = new Post(member2, board2, "엄마는 나 안 보고 싶을 수도 있어", "사실 그럴 가능성이 더 큰 편이죠", LocalDateTime.now(), 0, "거부", "거부");
+		postRepository.saveAll(Arrays.asList(club1, club2, club3));
 
 
 
