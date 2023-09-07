6f0855a1f38a05ca7421dceec4f927d47c6f2909
e22222yn <e22222yn@gmail.com>
2023-09-07 11:43:37 +0900

    게시판 번호별 조회하는 방법 clubController에서 getClub 참고해주세요~ setBoardNumber 안의 숫자만 해당하는 게시판 번호로 바꿔주시면 됩니다. Long 타입이니까 L 붙여주시구요.

---
+++ source/src/main/java/RainbowLike/repository/ClubRepository.java
@@ -0,0 +1,15 @@
+package RainbowLike.repository;
+
+import RainbowLike.entity.Board;
+import RainbowLike.entity.Post;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.List;
+
+public interface ClubRepository extends CrudRepository<Post, Long>
+{
+    List<Post> findByBoard(Board board);
+//    @Query("select p from post as p where p.board_num = 10")
+//    Iterable<Post> findByClubs();
+
+}
--- source/src/main/java/RainbowLike/service/BoardService.java
+++ source/src/main/java/RainbowLike/service/BoardService.java
@@ -3,6 +3,8 @@ package RainbowLike.service;
 import RainbowLike.dto.BoardDto;
 import RainbowLike.entity.Board;
 import RainbowLike.repository.BoardRepository;
+import RainbowLike.repository.ClubRepository;
+import RainbowLike.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
@@ -18,7 +20,8 @@ public class BoardService {
     private final ModelMapper mapper;
 
     private final BoardRepository boardRepository;
-
+    private final PostRepository postRepository;
+    private final ClubRepository clubRepository;
 
     public void createBoards(ArrayList<BoardDto> boardDtoList) {
 
@@ -29,5 +32,8 @@ public class BoardService {
             boardRepository.save(board);
         }
     }
+
+
+
 }
 
