248e97418c16542b946673d95b2f06317278edfa
mediny <mediny0613@gmail.com>
2023-09-11 16:07:08 +0900

    no message

--- source/src/front/src/App.js
+++ source/src/front/src/App.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 
 import './App.css';
 import './layout/css/font.css';
@@ -7,34 +7,18 @@ import Footer from "./footer/footer";
 import Header from "./Header/header";
 import Mypage_active from "./mypage/mypage_active";
 import Mypage from "./mypage/mypage";
-=======
-import {Route, Routes} from "react-router-dom";
 import './App.css';
-import Home from "./pages/Home";
 import React from "react";
-import LoginPage from "./pages/Login";
-import Admin from "./pages/Admin";
->>>>>>> 88d2d4886d3552068a9986ea1eafdfda596bc17d
 
 function App() {
 
   return (
-<<<<<<< HEAD
     <div className="App">
         {/*<Header/>*/}
         <Container_common/>
         <Mypage_active/>
-        {/*<Footer/>*/}
+        <Footer/>
     </div>
-=======
-      <div className="App">
-          <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/admin" element={<Admin />} />
-              <Route path="/login" element={<LoginPage />} />
-          </Routes>
-      </div>
->>>>>>> 88d2d4886d3552068a9986ea1eafdfda596bc17d
   );
 }
 
