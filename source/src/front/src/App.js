f701ddc7c1387d4d28cd8207b8b4fed91afd1e0d
mediny <mediny0613@gmail.com>
2023-09-11 17:50:33 +0900

    space 데이터 수정

--- source/src/front/src/App.js
+++ source/src/front/src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import './layout/css/font.css';
 import Footer from "./footer/footer";
- import Container_common from "./mypage/container_common";
+import Container_common from "./mypage/container_common";
 import Header from "./Header/header";
 import Mypage_active from "./mypage/mypage_active";
 import Mypage from "./mypage/mypage";
@@ -14,24 +14,22 @@ import React from "react";
 
 function App() {
 
-  return (
-<<<<<<< HEAD
-      <div className="App">
-          <Routes>
-              <Route path="/admin" element={<AdminPage />} />
-              <Route path="/login" element={<LoginPage />} />
-              <Route path="/singUp" element={<SingUp />} />
-          </Routes>
-      </div>
-=======
-    <div className="App">
-        {/*<Header/>*/}
-        <Container_common/>
-        <Mypage_active/>
-        <Footer/>
-    </div>
->>>>>>> 2885b1e61ffec65e6707cb99963a6cf5eee3b6f1
-  );
+    return (
+
+        <div className="App">
+            <Routes>
+                <Route path="/admin" element={<AdminPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/singUp" element={<SingUp/>}/>
+                {/*<Header/>*/}
+                <Container_common/>
+                <Mypage_active/>
+                <Footer/>
+            </Routes>
+        </div>
+
+
+    );
 }
 
 export default App;
