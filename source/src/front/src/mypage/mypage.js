1f0ee10d5ed08b869e38a4e6e2ae74abf4b6f9b3
mediny <mediny0613@gmail.com>
2023-09-13 19:44:21 +0900

    no message

--- source/src/front/src/mypage/mypage.js
+++ source/src/front/src/mypage/mypage.js
@@ -1,11 +1,12 @@
 
 import Container_common from './top';
+import Mypage_active from "./mypage_active";
 
 const Mypage=()=>{
     return(
         <main>
         <Container_common/>
-
+        {/*<Mypage_active/>*/}
         </main>
     );
 }
--- source/src/main/java/RainbowLike/repository/SpaceRepository.java
+++ source/src/main/java/RainbowLike/repository/SpaceRepository.java
@@ -1,10 +1,6 @@
 package RainbowLike.repository;
 
-<<<<<<< HEAD
-=======
-import RainbowLike.entity.Edu;
->>>>>>> 3db001eb08878d49e7a560a9a7c6a636782027da
-import RainbowLike.entity.Member;
+
 import RainbowLike.entity.Space;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -12,15 +8,11 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface SpaceRepository extends JpaRepository<Space,Long> {
 
-<<<<<<< HEAD
 
-    Space findBySpaceName(String spaceName);
-=======
-    Space findBySpaceNum(Long sapceNum);
 
     Space findTopByOrderBySpaceNumDesc();
 
 
 
->>>>>>> 3db001eb08878d49e7a560a9a7c6a636782027da
+
 }
