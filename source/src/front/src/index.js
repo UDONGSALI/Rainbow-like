3911e7576f11a15d398e47e493a096da77289e61
hehehol1372 <heheho1372@gmail.com>
2023-09-13 19:46:32 +0900

    no message

--- source/src/front/src/index.js
+++ source/src/front/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+<<<<<<< HEAD
 import { BrowserRouter } from 'react-router-dom';
 
 
@@ -11,3 +12,16 @@ root.render(
         <App />
     </BrowserRouter>
 );
+=======
+import { BrowserRouter } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+<BrowserRouter>
+<App />
+</BrowserRouter>
+);
+
+
+>>>>>>> bc06ba3 (no message)
