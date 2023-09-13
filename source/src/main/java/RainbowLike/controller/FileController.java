3bf541df6a4729c41e7bfef22b437c24534d22ad
UDONGSALI <129227923+UDONGSALI@users.noreply.github.com>
2023-09-13 17:11:22 +0900

    구글 클라우드 서비스에 파일 저장하기 완료, 불러오기도 완료, 기존에 불러올 시 a 태그에 download 속성을 이용하여 이름을 편리하게 바꿀 수 있었으나 현재 불가능 해 클라우드에 폴더 만들고 원본 이름으로 보존하는 방향으로 감

--- source/src/main/java/RainbowLike/controller/FileController.java
+++ source/src/main/java/RainbowLike/controller/FileController.java
@@ -2,8 +2,14 @@ package RainbowLike.controller;
 
 import RainbowLike.entity.*;
 import RainbowLike.repository.*;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.cloud.storage.*;
+import io.grpc.Context;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.WritableResource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -32,6 +38,13 @@ public class FileController {
 
     private final FileRepository fileRepository;
 
+
+
+    @Value("${spring.cloud.gcp.storage.bucket}") // application.yml에 써둔 bucket 이름
+    private String bucketName;
+
+
+
     @GetMapping
     private Iterable<File> getMembers() {
         return  fileRepository.findAll();
@@ -45,65 +58,144 @@ public class FileController {
         Edu edu = null;
         Post post = null;
 
-        // 여러 파일을 처리하는 코드
-        for (MultipartFile file : files) {
-            try {
+        String midPath = "";
 
-                // UUID 생성
-                UUID uuid = UUID.randomUUID();
+        // tableName에 따라 적절한 엔티티 검색
+        if (tableName.equals("member")) {
+            member = memberRepository.findTopByOrderByMemNumDesc();
+            midPath = tableName + "/" + member.getMemNum()+ "/";
+        }
+        if (tableName.equals("space")) {
+            space = spaceRepository.findTopByOrderBySpaceNumDesc();
+            midPath = tableName + "/" + space.getSpaceNum()+ "/";
+        }
+        if (tableName.equals("edu")) {
+            edu = eduRepository.findTopByOrderByEduNumDesc();
+            midPath = tableName  + "/" + edu.getEduNum()+ "/";
+        }
+        if (tableName.equals("post")) {
+            post = postRepository.findTopByOrderByPostNumDesc();
+            midPath = tableName + "/" + post.getPostNum()+ "/";
+        }
 
-                // 파일 저장 경로 설정
-                String originalFileName = file.getOriginalFilename();
-                String fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
-                String newFileName = uuid.toString() + fileExtension;
-                Path filePath = Path.of(dir, newFileName);
 
-                // 폴더가 없으면 폴더를 생성
-                if (!Files.exists(filePath.getParent())) {
-                    try {
-                        Files.createDirectories(filePath.getParent());
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        return ResponseEntity.status(500).body("폴더 생성 실패");
-                    }
-                }
+//        // 컴퓨터에 저장하는 메소드
+//        for (MultipartFile file : files) {
+//            try {
+//
+//                // UUID 생성
+//                UUID uuid = UUID.randomUUID();
+//
+//                // 파일 저장 경로 설정
+//                String originalFileName = file.getOriginalFilename();
+//                String fileExtension = originalFileName.substring(originalFileName.lastIndexOf("."));
+//                String newFileName = uuid.toString() + fileExtension;
+//                Path filePath = Path.of(dir, newFileName);
+//
+//
+//                // 폴더가 없으면 폴더를 생성
+//                if (!Files.exists(filePath.getParent())) {
+//                    try {
+//                        Files.createDirectories(filePath.getParent());
+//                    } catch (IOException e) {
+//                        e.printStackTrace();
+//                        return ResponseEntity.status(500).body("폴더 생성 실패");
+//                    }
+//                }
+//
+//                // 파일을 지정된 경로로 복사
+//                Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+//
+//                // 업로드된 파일 정보를 데이터베이스에 저장
+//                File createfile = new File();
+//                createfile.setFileOriName(file.getOriginalFilename());
+//                createfile.setFileName(newFileName);
+//                createfile.setFileUri(filePath.toString());
+//
+//                // tableName에 따라 적절한 엔티티 검색
+//                if (tableName.equals("member")) {
+//                    member = memberRepository.findTopByOrderByMemNumDesc();
+//                }
+//                if (tableName.equals("space")) {
+//                    space = spaceRepository.findTopByOrderBySpaceNumDesc();
+//                }
+//                if (tableName.equals("edu")) {
+//                    edu = eduRepository.findTopByOrderByEduNumDesc();
+//                }
+//                if (tableName.equals("post")) {
+//                    post = postRepository.findTopByOrderByPostNumDesc();
+//                }
+//
+//                // 파일과 관련된 엔티티 설정 및 저장
+//                createfile.setMember(member);
+//                createfile.setSpace(space);
+//                createfile.setEdu(edu);
+//                createfile.setPost(post);
+//
+//                fileRepository.save(createfile);
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//                return ResponseEntity.status(500).body("파일 업로드 실패");
+//            }
+//        }
+//        return ResponseEntity.ok("파일 업로드 성공");
 
-                // 파일을 지정된 경로로 복사
-                Files.copy(file.getInputStream(), filePath, StandardCopyOption.REPLACE_EXISTING);
+        try {
+
+            // 구글 클라우드 서비스 설정
+            ClassPathResource resource = new ClassPathResource("rainbow-like-6e3171ac1695.json");
+            GoogleCredentials credentials = GoogleCredentials.fromStream(resource.getInputStream());
+            String projectId = "rainbow-like";
+            Storage storage = StorageOptions.newBuilder()
+                    .setProjectId(projectId)
+                    .setCredentials(credentials)
+                    .build()
+                    .getService();
+
+
+            for (MultipartFile file : files) {
+//                // UUID를 생성하여 파일 이름을 고유하게 만듭니다.
+//                String uuid = UUID.randomUUID().toString();
+//                // 파일 확장자 가져오기
+//                String ext = file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+
+
+                // 파일 이름 변경
+                String newFileName = midPath + file.getOriginalFilename() ;
+                // 파일 url
+                String fileUrl = "https://storage.googleapis.com/" + bucketName + "/" + newFileName;
+
+                // BlobInfo 객체 생성
+                BlobInfo blobInfo = BlobInfo.newBuilder(bucketName, newFileName).build();
+
+                // GCS에 파일 업로드
+                Blob blob =storage.create(blobInfo, file.getBytes());
+
 
                 // 업로드된 파일 정보를 데이터베이스에 저장
                 File createfile = new File();
                 createfile.setFileOriName(file.getOriginalFilename());
                 createfile.setFileName(newFileName);
-                createfile.setFileUri(filePath.toString());
+                createfile.setFileUri(fileUrl.toString());
 
-                // tableName에 따라 적절한 엔티티 검색
-                if (tableName.equals("member")) {
-                    member = memberRepository.findTopByOrderByMemNumDesc();
-                }
-                if (tableName.equals("space")) {
-                    space = spaceRepository.findTopByOrderBySpaceNumDesc();
-                }
-                if (tableName.equals("edu")) {
-                    edu = eduRepository.findTopByOrderByEduNumDesc();
-                }
-                if (tableName.equals("post")) {
-                    post = postRepository.findTopByOrderByPostNumDesc();
-                }
-
-                // 파일과 관련된 엔티티 설정 및 저장
+                // 외래키 저장
                 createfile.setMember(member);
                 createfile.setSpace(space);
                 createfile.setEdu(edu);
                 createfile.setPost(post);
 
                 fileRepository.save(createfile);
-
-            } catch (IOException e) {
-                e.printStackTrace();
-                return ResponseEntity.status(500).body("파일 업로드 실패");
             }
+
+
+            return ResponseEntity.ok("파일 업로드 성공");
+        } catch (IOException e) {
+            e.printStackTrace();
+            return ResponseEntity.status(500).body("파일 업로드 실패");
         }
-        return ResponseEntity.ok("파일 업로드 성공");
+
+
+
     }
 }
