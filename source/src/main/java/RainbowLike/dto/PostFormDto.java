2271f8d4781523efeb818e020c31e532e13c6d5a
e22222yn <e22222yn@gmail.com>
2023-09-15 19:30:57 +0900

    BaseEntity 수정(작성일자 자동기입) / comment Entity에 parentNum 추가 / test Comment 추가 / testPost 작성을 dto 경유로 변경

--- source/src/main/java/RainbowLike/dto/PostFormDto.java
+++ source/src/main/java/RainbowLike/dto/PostFormDto.java
@@ -1,16 +1,21 @@
 package RainbowLike.dto;
 
+import RainbowLike.entity.Board;
+import RainbowLike.entity.Member;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 
 @Getter
 @Setter
 public class PostFormDto {
 
     private Long memNum;
+    private Member member;
     private Long boardNum;
+    private Board board;
     private String title;
     private String content;
     private LocalDateTime writeDate;
@@ -39,10 +44,79 @@ public class PostFormDto {
         this.clubAllowStatus = clubAllowStatus;
         this.clubRecuStatus = clubRecuStatus;
         this.boardNum = boardNum;
-//        board.setBoardNum(boardNum);
         this.memNum = memNum;
-//        member.setMemNum(memNum);
     }
 
+    static public ArrayList<PostFormDto> createTestPost() {
+        ArrayList<PostFormDto> postList = new ArrayList<>();
+        Board board1 = new Board();
+        Board board2 = new Board();
+        board1.setBoardNum(9L);
+        board2.setBoardNum(8L);
+
+        Member member1 = new Member();
+        Member member2 = new Member();
+        member1.setMemNum(1L);
+        member2.setMemNum(2L);
+
+
+        PostFormDto post1 = new PostFormDto();
+        post1.setBoard(board1);
+        post1.setMember(member1);
+        post1.setTitle("테스트 게시글 1");
+        post1.setContent("테스트 게시글 본문 1");
+        post1.setPageView(0);
+        post1.setClubAllowStatus("허가");
+        post1.setClubRecuStatus("모집중");
+        postList.add(post1);
+
+        PostFormDto post2 = new PostFormDto();
+        post2.setBoard(board1);
+        post2.setMember(member1);
+        post2.setTitle("테스트 게시글 2");
+        post2.setContent("테스트 게시글 본문 2");
+        post2.setWriteDate(LocalDateTime.now());
+        post2.setPageView(0);
+        post2.setClubAllowStatus("허가");
+        post2.setClubRecuStatus("진행중");
+        postList.add(post2);
+
+        PostFormDto post3 = new PostFormDto();
+        post3.setBoard(board1);
+        post3.setMember(member2);
+        post3.setTitle("테스트 게시글 3");
+        post3.setContent("테스트 게시글 본문 3");
+        post3.setWriteDate(LocalDateTime.now());
+        post3.setPageView(0);
+        post3.setClubAllowStatus("거부");
+        post3.setClubRecuStatus("거부");
+        postList.add(post3);
+
+
+        PostFormDto post4 = new PostFormDto();
+        post4.setBoard(board2);
+        post4.setMember(member1);
+        post4.setTitle("상담 테스트 게시글 1");
+        post4.setContent("상담 테스트 게시글 본문 1");
+        post4.setWriteDate(LocalDateTime.now());
+        post4.setPageView(0);
+        post4.setConsField("상담대기");
+
+        postList.add(post4);
+
+
+        PostFormDto post5 = new PostFormDto();
+        post5.setBoard(board2);
+        post5.setMember(member2);
+        post5.setTitle("상담 테스트 게시글 2");
+        post5.setContent("상담 테스트 게시글 본문 2");
+        post5.setWriteDate(LocalDateTime.now());
+        post5.setPageView(0);
+        post5.setConsField("상담완료");
+
+        postList.add(post5);
+
+        return postList;
+    }
 
 }
--- source/src/main/java/RainbowLike/service/PostService.java
+++ source/src/main/java/RainbowLike/service/PostService.java
@@ -1,12 +1,17 @@
 package RainbowLike.service;
 
+import RainbowLike.dto.PostFormDto;
 import RainbowLike.entity.Post;
 import RainbowLike.repository.ClubRepository;
 import RainbowLike.repository.PostRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
 
 @Service
 @Transactional
@@ -17,12 +22,21 @@ public class PostService {
     ClubRepository clubRepository;
     @Autowired
     PostRepository postRepository;
+    private final ModelMapper mapper;
 
 public void savePost(Post post) {
 
         postRepository.save(post);
 }
 
+    public void createPosts(ArrayList<PostFormDto> postList){
+        for(PostFormDto postFormDto : postList){
+            mapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+            Post post = mapper.map(postFormDto, Post.class);
+
+            postRepository.save(post);
+        }
+    }
     @Transactional
     public int updatePageView(Long id) {
     return postRepository.updateView(id);
