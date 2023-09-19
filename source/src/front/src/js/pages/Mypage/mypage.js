defc3cb63695c6f95901025c94039a05251fb4df
hehehol1372 <heheho1372@gmail.com>
2023-09-19 10:17:31 +0900

    no message

--- source/src/front/src/js/pages/Mypage/mypage.js
+++ source/src/front/src/js/pages/Mypage/mypage.js
@@ -1,20 +1,25 @@
 
-
-/*
+<<<<<<< HEAD
 import Top from '../components/mypage/top';
-
+import Mypage_active from "../components/mypage/mypage_active";
 import Footer from "../layouts/footer";
+=======
 import Container_common from './top';
+>>>>>>> ab3dd0c (no message)
 
 const Mypage=()=>{
     return(
         <main>
+<<<<<<< HEAD
         <Top/>
-        //<Mypage_active/>
+        <Mypage_active/>
         <Footer/>
+=======
         <Container_common/>
+
+>>>>>>> ab3dd0c (no message)
         </main>
     );
 }
 
-export default Mypage;*/
+export default Mypage;
