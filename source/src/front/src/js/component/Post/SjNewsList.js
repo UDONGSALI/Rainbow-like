f9235ea847e82864e3dd2b501dbb6d0894f2fa5e
UDONGSALI <udongsali77@gmail.com>
2023-09-23 17:36:41 +0900

    충돌해결

--- source/src/front/src/js/component/Post/SjNewsList.js
+++ source/src/front/src/js/component/Post/SjNewsList.js
@@ -1,19 +1,14 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { SERVER_URL } from '../Common/constants';
 import styles from '../../../css/component/Post/SjNewList.module.css';
-<<<<<<< HEAD
-import Pagination from "../Common/Pagination"; // StyledReactJsPagination 컴포넌트를 import
-=======
-import ActionBtn from "../ActionBtn";
->>>>>>> e36907b47623abf4bceba347f46cdefafa050d63
+import Pagination from "../Common/Pagination";
 
 function SjNewsList(props) {
     const { boardNum } = props;
     const [posts, setPosts] = useState([]);
     const [files, setFiles] = useState([]);
     const navigate = useNavigate();
-<<<<<<< HEAD
     const [currentPage, setCurrentPage] = useState(1);
     const postsPerPage = 6;
 
@@ -21,61 +16,25 @@ function SjNewsList(props) {
         fetch(`${SERVER_URL}posts?boardNum=${boardNum}`)
             .then(response => response.json())
             .then(data => {
-=======
-    const [currentPage, setCurrentPage] = useState(1); // 페이징
-    const postsPerPage = 6; // 게시글 개수
-
-    useEffect(() => {
-        // boardNum이 유효한 경우에만 게시글 목록을 가져오는 API 호출
-        fetch(`${SERVER_URL}posts?boardNum=${boardNum}`)
-            .then(response => response.json())
-            .then(data => {
-                // boardNum이 3인 게시글만 필터링하여 저장
->>>>>>> e36907b47623abf4bceba347f46cdefafa050d63
                 const filteredPosts = data.filter(post => post.board.boardNum === 3);
                 setPosts(filteredPosts);
             })
             .catch(error => console.error(error));
 
-<<<<<<< HEAD
-=======
-        // 파일 정보 가져오기
->>>>>>> e36907b47623abf4bceba347f46cdefafa050d63
         fetch(SERVER_URL + `files`)
             .then(response => response.json())
             .then(data => setFiles(data))
             .catch(error => console.error(error));
+
     }, [boardNum]);
-
-<<<<<<< HEAD
-=======
-    // 현재 페이지에 따라 게시글 목록 필터링
->>>>>>> e36907b47623abf4bceba347f46cdefafa050d63
-    const indexOfLastPost = currentPage * postsPerPage;
-    const indexOfFirstPost = indexOfLastPost - postsPerPage;
-    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
-
-<<<<<<< HEAD
-=======
-    // 페이지 변경 시 호출할 함수
->>>>>>> e36907b47623abf4bceba347f46cdefafa050d63
-    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     const onDelClick = (postNum) => {
         fetch(`${SERVER_URL}api/posts/${postNum}`, { method: 'DELETE' })
             .then(response => {
-<<<<<<< HEAD
-=======
-                // 삭제 성공 시, 게시글 목록 다시 불러오기
->>>>>>> e36907b47623abf4bceba347f46cdefafa050d63
                 fetch(`${SERVER_URL}posts?boardNum=${boardNum}`)
                     .then(response => response.json())
                     .then(data => setPosts(data))
                     .catch(error => console.error(error));
-<<<<<<< HEAD
-=======
-
->>>>>>> e36907b47623abf4bceba347f46cdefafa050d63
             })
             .catch(err => console.error(err));
     };
@@ -84,14 +43,16 @@ function SjNewsList(props) {
         navigate(`/posts/edit/${postNum}`);
     };
 
-<<<<<<< HEAD
-=======
-    // 각 게시물당 하나의 이미지만 가져오기
->>>>>>> e36907b47623abf4bceba347f46cdefafa050d63
     const getPostImage = (postNum) => {
         const matchingFile = files.find(file => file.post && file.post.postNum === postNum);
         return matchingFile ? matchingFile.fileUri : '';
     };
+
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+
+    const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
     return (
         <div>
@@ -101,11 +62,7 @@ function SjNewsList(props) {
                         <div className={styles.leftTop}>
                             <img
                                 src={getPostImage(post.postNum)}
-<<<<<<< HEAD
-                                alt={`이미지 ${index + 1}`}
-=======
-                                alt={`Image 1`}
->>>>>>> e36907b47623abf4bceba347f46cdefafa050d63
+                                alt={`Image ${index + 1}`}
                                 className={styles.postImage}
                             />
                         </div>
@@ -118,8 +75,6 @@ function SjNewsList(props) {
                     </div>
                 ))}
             </div>
-<<<<<<< HEAD
-            {/* StyledReactJsPagination 컴포넌트를 사용 */}
             <Pagination
                 activePage={currentPage}
                 itemsCountPerPage={postsPerPage}
@@ -134,13 +89,12 @@ function SjNewsList(props) {
                     <button onClick={() => onEditClick(posts[0].postNum)} className={styles.postEditButton}>
                         수정
                     </button>
-                    <button onClick={() => onDelClick(posts[0].postNum)} className={styles.postDeleteButton}>
+                    <button onClick={() => onDelClick(posts[0].postNum)}
+                            className={styles.postDeleteButton}>
                         삭제
                     </button>
                 </div>
             )}
-=======
-            {/* 페이징 목록 추가 */}
             <div className={styles.pagination}>
                 {Array.from({ length: Math.ceil(posts.length / postsPerPage) }, (_, index) => (
                     <button key={index} onClick={() => paginate(index + 1)}>
@@ -148,12 +102,6 @@ function SjNewsList(props) {
                     </button>
                 ))}
             </div>
-            {/* 수정 및 삭제 버튼*/}
-            <ActionBtn
-                onEditClick={() => onEditClick(currentPosts[0].postNum)}
-                onDeleteClick={() => onDelClick(currentPosts[0].postNum)}
-            />
->>>>>>> e36907b47623abf4bceba347f46cdefafa050d63
         </div>
     );
 }
