b0ed4f861cf4dd1531a49d93404bea82b1643c35
hehehol1372 <heheho1372@gmail.com>
2023-09-26 18:40:53 +0900

    페이징 css 수정 페이지에 Footer 추가 PostNoticeList style 수정 path /post/:boardNum, /imgPost/:boardNum 추가

--- source/src/front/src/App.js
+++ source/src/front/src/App.js
@@ -4,8 +4,6 @@ import LoginPage from "./js/pages/Login/LoginPage";
 import React, {useEffect} from "react";
 import MemManagePage from "./js/pages/Member/MemManagePage";
 import NavBar from './js/layout/Navbar/NavBar';
-import SingUp from "./js/component/Login/SignUp";
-import EduList from "./js/component/Edu/EduList";
 import EduCalendarPage from "./js/pages/Edu/EduCalendarPage";
 import EduListPage from "./js/pages/Edu/EduListPage";
 import EduDetailPage from "./js/pages/Edu/EduDetailPage";
@@ -15,18 +13,34 @@ import EduApplyPage from "./js/pages/Edu/EduApplyPage";
 import PostDetailPage from './js/pages/Post/PostDetailPage';
 import NoticeListPage from './js/pages/Post/NoticeListPage';
 import Main from "./js/component/screens/Main";
-import EduManagePage from '../src/js/pages/Member/MemManagePage';
 import SjNewsPage from "./js/pages/Post/SjNewsPage";
 import PostList from "./js/component/Post/PostList";
 import RentPage from "./js/pages/Rent/RentPage";
 import RentStatusPage from "./js/pages/Rent/RentStatusPage";
 import RentApplicationPage from "./js/pages/Rent/RentApplicationPage";
+import FTMainPage from "./js/pages/FT/FTMainPage";
+import FTWListPage from "./js/pages/FT/FTW/FTWListPage";
+import FTWFormPage from "./js/pages/FT/FTW/FTWFormPage";
+import FTWDtlPage from "./js/pages/FT/FTW/FTWDtlPage";
+import FTWEditPage from "./js/pages/FT/FTW/FTWEditPage";
+import FTCListPage from "./js/pages/FT/FTC/FTCListPage";
+import FTCFormPage from "./js/pages/FT/FTC/FTCFormPage";
+import FTCDtlPage from "./js/pages/FT/FTC/FTCDtlPage";
+import FTCEditPage from "./js/pages/FT/FTC/FTCEditPage";
+import FTMListPage from "./js/pages/FT/FTM/FTMListPage";
+import MatchingPopup from "./js/component/FT/FTM/MatchingPopup";
+import ClubPage from "./js/pages/Club/ClubPage";
+import ClubFormPage from "./js/pages/Club/ClubFormPage";
 import SignUpPage from "./js/pages/Login/SignUpPage";
 import ClubDtlPage from "./js/pages/Club/ClubDtlPage";
+import ClubEditorPage from "./js/pages/Club/ClubEditorPage";
+import EduApplyCheckPage from "./js/pages/Edu/EduApplyCheckPage";
+import LaborListPage from "./js/pages/Post/LaborListPage";
+
 
 function App() {
     const isAdmin = sessionStorage.getItem("role") === "ADMIN"; // 사용자가 ADMIN인지 확인
-    const memId =  sessionStorage.getItem("memId");
+    const memId = sessionStorage.getItem("memId");
     const navigate = useNavigate();
 
     useEffect(() => {
@@ -64,27 +78,44 @@ function App() {
         <div className="App">
             <NavBar/>
             <Routes>
-                <Route path="/" element={<Main/>} />
+                <Route path="/" element={<Main/>}/>
                 <Route path="/admin/member" element={isAdmin ? <MemManagePage/> : null}/>
-                <Route path="/admin/edu" element={isAdmin ? <EduManagePage/> : null}/>
+                <Route path="/admin/edu" element={isAdmin ? <EduListPage/> : null}/>
                 <Route path="/admin/edu/add" element={isAdmin ? <EduAddPage/> : null}/>
                 <Route path="/admin/edu/edit/:eduNum" element={isAdmin ? <EduEditPage/> : null}/>
                 <Route path="/login" element={<LoginPage/>}/>
-                <Route path="/singUp" element={<SignUpPage/>}/>
-                <Route path="/edu/list" element={<EduList/>}/>
+                <Route path="/signUp" element={<SignUpPage/>}/>
                 <Route path="/edu/list" element={<EduListPage/>}/>
                 <Route path="/edu/calendar" element={<EduCalendarPage/>}/>
                 <Route path="/edu/detail/:eduNum" element={<EduDetailPage/>}/>
                 <Route path="/edu/apply/:eduNum" element={<EduApplyPage/>}/>
+                <Route path="/edu/applyck" element={<EduApplyCheckPage memId={memId}/>}/>
+                <Route path="/sj" element={<SjNewsPage/>}/>
                 <Route path="/rent" element={<RentPage/>}/>
                 <Route path="/rent/rentStatus" element={<RentStatusPage/>}/>
                 <Route path="/rent/rentApplication/:spaceNum" element={<RentApplicationPage/>}/>
-                <Route path="/posts" element={<PostList />}   />
+                <Route path="/posts" element={<PostList/>}/>
                 <Route path="/edu/apply/:eduNum" element={<EduApplyPage/>}/>
-
+                <Route path="/sj" element={<SjNewsPage/>}/>
+                <Route path="/clubs" element={<ClubPage/>}/>
+                <Route path="/clubs/new" element={<ClubFormPage/>}/>
+                <Route path="/clubs/:id" element={<ClubDtlPage/>}/>
+                <Route path="/clubs/edit/:id" element={<ClubEditorPage/>}/>
+                <Route path="/ftmain" element={<FTMainPage/>}/>
+                <Route path="/ftw" element={<FTWListPage/>}/>
+                <Route path="/ftw/new" element={<FTWFormPage/>}/>
+                <Route path="/ftw/:id" element={<FTWDtlPage/>}/>
+                <Route path="/ftw/edit/:id" element={<FTWEditPage/>}/>
+                <Route path="/ftc" element={<FTCListPage/>}/>
+                <Route path="/ftc/new" element={<FTCFormPage/>}/>
+                <Route path="/ftc/:id" element={<FTCDtlPage/>}/>
+                <Route path="/ftc/edit/:id" element={<FTCEditPage/>}/>
+                <Route path="/ftm" element={<FTMListPage/>}/>
+                <Route path="/ftmpop/:speField/:ftcNum" element={<MatchingPopup/>}/>
                 <Route path="/post/detail/:postNum" element={<PostDetailPage/>}/>
                 <Route path="/imgPost/:boardNum" element={<SjNewsPage/>}/>
                 <Route path="/post/:boardNum" element={<NoticeListPage/>}/>
+                <Route path="/labor" element={<LaborListPage/>}/>
             </Routes>
         </div>
     )
