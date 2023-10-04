0b3c0fab457dd9d03302ba86f471d18ecb4e7d61
UDONGSALI <129227923+UDONGSALI@users.noreply.github.com>
2023-10-04 09:27:04 +0900

    no message

--- source/src/front/src/js/pages/Login/LoginPage.js
+++ source/src/front/src/js/pages/Login/LoginPage.js
@@ -1,14 +1,15 @@
 import React from 'react';
-import {AppBar, Toolbar, Typography} from "@mui/material";
 import Login from "../../component/Login/Login";
 import UrlComponent from "../../layout/Header/UrlComponent";
 import {headerInfo, urlData} from "../../layout/Header/Data/LoginHeader";
 
 const LoginPage = () => {
+    const memId = sessionStorage.getItem("memId");
+
     return (
         <div>
             <UrlComponent headerTitle={headerInfo} urlItems={urlData} footerTitle={'Login'}/>
-            <Login />
+            <Login memId={memId} />
         </div>
     );
 };
