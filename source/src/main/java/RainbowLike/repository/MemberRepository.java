7a8948ab2c75f6b555df2acda7d13783f8c1fd13
e22222yn <e22222yn@gmail.com>
2023-10-05 17:55:05 +0900

    단체 메시지 전송 기능 완료

--- source/src/main/java/RainbowLike/repository/MemberRepository.java
+++ source/src/main/java/RainbowLike/repository/MemberRepository.java
@@ -5,11 +5,9 @@ import RainbowLike.constant.Type;
 import RainbowLike.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
@@ -31,5 +29,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Member findTopByOrderByMemNumDesc();
 
 
-
+    @Query("SELECT m.tel FROM Member m")
+    List<String> findAllTels();
 }
