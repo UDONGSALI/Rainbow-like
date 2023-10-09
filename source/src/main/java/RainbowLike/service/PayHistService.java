dbbb8d767295e1963f6b95a0c34a9e8457899019
UDONGSALI <udongsali77@gmail.com>
2023-10-09 23:07:09 +0900

    포스트에 배정 상담사 추가, 상태 enum 타입으로 변경하여 상담  게시글 작성 시 wait 으로 들어가게 하고, 답변 글 작성 시 상태 변경하게 해야 함, 교육, 대여 상태 변경과 상담사 배정시 문자 전송

--- source/src/main/java/RainbowLike/service/PayHistService.java
+++ source/src/main/java/RainbowLike/service/PayHistService.java
@@ -7,12 +7,16 @@ import RainbowLike.dto.PaymentAndStatusChangeResult;
 import RainbowLike.entity.EduHist;
 import RainbowLike.entity.PayHist;
 import RainbowLike.entity.RentHist;
+import RainbowLike.repository.MemberRepository;
 import RainbowLike.repository.PayHistRepository;
 import RainbowLike.repository.RentHistRepository;
+import RainbowLike.repository.SpaceRepository;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Optional;
 
 @Service
@@ -21,14 +25,35 @@ public class PayHistService {
 
     private final PayHistRepository payHistRepository;
     private final RentHistRepository rentHistRepository;
+    private final SpaceRepository spaceRepository;
+    private final MemberRepository memberRepository;
     private final RentHistService rentHistService;
-
+    private final SmsService smsService;
     public Iterable<PayHist> findAll() {
         return payHistRepository.findAll();
     }
 
+    public Iterable<PayHist> searchByOptionAndValue(String option, String value) {
+        Iterable<PayHist> result;
+        switch (option) {
+            case "rentHistNum":
+                result = payHistRepository.findByRentHist(rentHistRepository.findByRentHistNum(Long.parseLong(value)));
+                break;
+            case "spaceName":
+                result = payHistRepository.findByRentHistIn(rentHistRepository.findBySpaceIn(spaceRepository.findBySpaceNameContaining(value)));
+                break;
+            case "memId":
+                result = payHistRepository.findByRentHistIn(rentHistRepository.findByMemberIn(memberRepository.findByMemIdContaining(value)));
+                break;
+            default:
+                result = new ArrayList<>();
+        }
+        return result;
+    }
+
+
     @Transactional
-    public PaymentAndStatusChangeResult processPaymentAndStatusChange(PayHistDto payHistDto) {
+    public PaymentAndStatusChangeResult addPayHist(PayHistDto payHistDto) {
 
         PayHist payHist = new PayHist();
         RentHist rentHist = rentHistRepository.findByRentHistNum(payHistDto.getRentHistNum());
@@ -36,9 +61,12 @@ public class PayHistService {
         payHist.setFee(payHistDto.getFee());
 
         payHistRepository.save(payHist);
+        smsService.rentPaySms(payHistDto.getRentHistNum());
 
         RentHist updatedRentHist = rentHistService.updateRentHistPayStatus(rentHist);
 
         return new PaymentAndStatusChangeResult(payHist, updatedRentHist);
     }
+
+
 }
