a50f77c856027956ee1921fe98cf641ae9b2c75f
UDONGSALI <129227923+UDONGSALI@users.noreply.github.com>
2023-10-10 16:44:56 +0900

    로그 통계 기능 완료, 회원가입 페이지 약관 체크 박스 둘다 클릭 시 회원가입 컴포넌트로 교체하는 형식으로 변경 함

--- source/src/front/src/js/pages/Login/SignUpPage.js
+++ source/src/front/src/js/pages/Login/SignUpPage.js
@@ -1,35 +1,53 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import SignUp from "../../component/Login/SignUp";
 import SignAgreement from "../../component/Login/SignAgreement";
 import UrlComponent from "../../layout/Header/UrlComponent";
-import {headerInfo, urlData} from "../../layout/Header/Data/LoginHeader";
+import { headerInfo, urlData } from "../../layout/Header/Data/LoginHeader";
+import styles from '../../../css/pages/Login/SignUpPage.module.css';
 
 function SignUpPage() {
     const [isAgreed, setIsAgreed] = useState({
         first: false,
         second: false
     });
+    const [showSignUp, setShowSignUp] = useState(false);
 
     const handleAgreement = (first, second) => {
-        setIsAgreed({first, second});
+        setIsAgreed({ first, second });
     };
 
-    const handleSignUpClick = () => {
-        if (!isAgreed.first || !isAgreed.second) {
-            alert('약관에 동의하지 않으면 회원가입이 불가능합니다.');
-
-        }
-        // 회원가입 처리 로직
+    const onAnimationEnd = () => {
+        setShowSignUp(true);
     };
 
     return (
-        <>
-            <UrlComponent headerTitle={headerInfo} urlItems={urlData} footerTitle={'회원가입'}/>
-            <div style={{display: 'flex', justifyContent: "center"}}>
-                <SignAgreement onAgreementChange={handleAgreement}/>
-                <SignUp onSignUpClick={handleSignUpClick}/>
+        <div>
+            <UrlComponent headerTitle={headerInfo} urlItems={urlData} footerTitle={'회원가입'} />
+            <div style={{
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: '60%',
+                margin: 'auto',
+                padding: '20px',
+                backgroundColor: '#f9f9f9',
+                boxShadow: '0 0 5px rgba(0, 0, 0, 0.2)',
+                borderRadius: '5px'
+            }}>
+
+                {!showSignUp ? (
+                    <div className={isAgreed.first && isAgreed.second ? styles.slideLeftOut : ''} onAnimationEnd={isAgreed.first && isAgreed.second ? onAnimationEnd : undefined}>
+                        <SignAgreement onAgreementChange={handleAgreement} />
+                    </div>
+                ) : (
+                    <div className={styles.slideRightIn}>
+                        <SignUp />
+                    </div>
+                )}
+
             </div>
-        </>
+        </div>
     );
 }
 
