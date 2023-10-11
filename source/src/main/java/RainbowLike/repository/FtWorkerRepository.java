0d072c35567611e4929914f38fef5376184195d7
mediny <mediny0613@gmail.com>
2023-10-11 09:17:40 +0900

    back - 마이페이지(인재풀신청내역)

--- source/src/main/java/RainbowLike/repository/FtWorkerRepository.java
+++ source/src/main/java/RainbowLike/repository/FtWorkerRepository.java
@@ -1,9 +1,15 @@
 package RainbowLike.repository;
 
 import RainbowLike.entity.FtWorker;
+import RainbowLike.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
-public interface FtWorkerRepository extends JpaRepository <FtWorker, Long> {
+public interface FtWorkerRepository extends JpaRepository<FtWorker, Long> {
+    List<FtWorker> findByMember(Member member);
+
 }
--- source/src/main/java/RainbowLike/repository/MemberRepository.java
+++ source/src/main/java/RainbowLike/repository/MemberRepository.java
@@ -32,4 +32,6 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
 
 
 
+
+
 }
