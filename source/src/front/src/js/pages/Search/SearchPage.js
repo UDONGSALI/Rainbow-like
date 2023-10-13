2fb32c3ee82e9183b197a53fc9f7ded90293b75e
UDONGSALI <129227923+UDONGSALI@users.noreply.github.com>
2023-10-13 21:10:10 +0900

    검색 페이지 작업 중

--- source/src/front/src/js/pages/Search/SearchPage.js
+++ source/src/front/src/js/pages/Search/SearchPage.js
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import UrlComponent from "../../layout/Header/UrlComponent";
 import {headerInfo, urlData} from "../../layout/Header/Data/SearchHeader";
+import SearchContainer from "../../component/Search/SearchContainer";
 
 function SearchPage() {
 
     return (
         <div>
             <UrlComponent headerTitle={headerInfo} urlItems={urlData} footerTitle={'통합 검색'} />
+            <SearchContainer/>
         </div>
     );
 }
