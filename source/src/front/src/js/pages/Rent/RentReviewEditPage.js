4ac44747d70da49bfa3984a1b60b9c3929a2fb34
mediny <mediny0613@gmail.com>
2023-10-16 21:29:07 +0900

    no message

--- source/src/front/src/js/pages/Rent/RentReviewEditPage.js
+++ source/src/front/src/js/pages/Rent/RentReviewEditPage.js
@@ -1,14 +1,14 @@
 import React from "react";
-import UrlComponent from "../../layout/Header/UrlComponent";
 import {headerInfo, urlData} from "../../layout/Header/Data/RentHeader";
 import RentReviewEdit from "../../component/Rent/RentReview/RentReviewEdit";
+import Header from "../../layout/Header/Header";
 
 
 
 function RentReviewEditPage() {
     return (
         <div id="rentBody">
-            <UrlComponent headerTitle={headerInfo} urlItems={urlData} footerTitle={'대관 이용 후기'}/>
+            <Header headerTitle={headerInfo} urlItems={urlData} footerTitle={'대관 이용 후기'}/>
             <RentReviewEdit/>
         </div>
     )
