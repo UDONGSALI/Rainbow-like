723897c12155419af124976c1f13dd00de5ea170
mediny <mediny0613@gmail.com>
2023-10-17 20:11:23 +0900

    공간대관-이용후기(리스트페이지(완)/포스트페이지(css수정중)/에디터페이지&글쓰기페이지(수정중))   /  대관신청페이지(수정중)

--- source/src/front/src/js/component/Rent/RentSpaceInfo.js
+++ source/src/front/src/js/component/Rent/RentSpaceInfo.js
@@ -3,71 +3,69 @@ import {useHistory, useLocation, useNavigate} from 'react-router-dom';
 import {SERVER_URL} from "../Common/constants";
 
 
-const LoginMember = () => {
-    const [member, setMember] = useState(null);
-    const location = useLocation();
-    const boardNum = location.state?.boardNum;
+const RentSpaceInfo = () => {
+    const [spaces, setSpaces] = useState([]);
     const navigate = useNavigate();
-    const memId = sessionStorage.getItem("memId");
-    const [formData, setFormData] = useState();
+    // const spaceNum = sessionStorage.getItem("spaceNum");
+
 
     useEffect(() => {
-        fetch(SERVER_URL + `members/id/${memId}`)
+        let spaceNum;
+        fetch(SERVER_URL + `/spaces/${spaceNum}`)
             .then(response => response.json())
             .then(data => {
-                setMember(data);
+                setSpaces(data);
 
             })
             .catch(error => {
-                alert('회원 정보를 찾을 수 없습니다!');
-                window.location.href = '/login';
+                alert('대관 장소 정보를 찾을 수 없습니다!');
+                // window.location.href = '/rent/status';
             });
     }, []);
 
     return (
         <div id="title">
-            {member !== null ? (  // member가 null이 아닌 경우에만 렌더링
                 <div className="memApplyInfo">
-                    <h3>신청자 정보</h3>
+                    <h3>대관 신청정보</h3>
                     <hr />
                     <ul>
                         <li>
                             <p>*</p>
-                            <b>신청자명</b> <input value={member.name} />
+                            <b>장소명</b> <input value={spaces.spaceName} />
                         </li>
                         <hr />
                         <li>
                             <p>*</p>
-                            <b>휴대폰번호</b> <input value={member.tel} />
+                            <b>사용날짜</b> <input value={spaces.tel} />
                         </li>
                         <hr />
                         <li>
                             <p>*</p>
-                            <b>이메일주소</b> <input value={member.email} />
+                            <b>사용시간</b> <input value={spaces.email} />
                         </li>
                         <hr />
                         <li>
-                            <b>단체명</b>
+                            <p>*</p>
+                            <b>대관료</b>
                             <input />
                         </li>
                         <hr />
                         <li>
-                            <b>대표자</b>
-                            <input value={member.name} />
+                            <p>*</p>
+                            <b>사용인원</b>
+                            <input value={spaces.name} />
                         </li>
                         <hr />
                         <li>
-                            <b>사업자등록증</b>
-                            <button>파일추가</button>
+                            <p>*</p>
+                            <b>사용목적</b>
+                            <input/>
                         </li>
                         <hr />
                     </ul>
                 </div>
-            ) : (
-                <p>멤버 정보를 불러오는 중이거나 로그인이 필요합니다.</p>
-            )}
         </div>
     );
 }
 
-export default LoginMember;
+export default RentSpaceInfo;
