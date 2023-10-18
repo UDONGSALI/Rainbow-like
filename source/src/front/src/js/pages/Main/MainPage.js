6007c902bff964b98b863e548a6147354732d4c3
UDONGSALI <129227923+UDONGSALI@users.noreply.github.com>
2023-10-18 14:13:33 +0900

    포스트 컨트롤러 고친데 오류 수정, 자잘한 기능 추가, 이제 게시글 리스트에 검색 추가 후 백엔드, 프론트 엔드 코드 정리

--- source/src/front/src/js/pages/Main/MainPage.js
+++ source/src/front/src/js/pages/Main/MainPage.js
@@ -1,8 +1,10 @@
 import Main from "../../component/Main/Main";
+import QuickMenu from "../../layout/QuickMenu/QuickMenu";
 
 function MainPage() {
     return (
         <div>
+            <QuickMenu useChat={true} useScrollTop={true}/>
             <Main/>
         </div>
     )
