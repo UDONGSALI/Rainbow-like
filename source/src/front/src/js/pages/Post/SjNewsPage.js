4c7ef9f608b3a806717a9bfdfd1105485fbc0b18
hehehol1372 <heheho1372@gmail.com>
2023-10-20 10:59:23 +0900

    no message

--- source/src/front/src/js/pages/Post/SjNewsPage.js
+++ source/src/front/src/js/pages/Post/SjNewsPage.js
@@ -9,11 +9,20 @@ import Header from "../../layout/Header/Header";
 function SjNewsPage() {
     const { boardNum } = useParams();
 
+    let footerTitle = "";
+
+    if (boardNum == "3") {
+        footerTitle = "세종시 단체 및 소식";
+    } else if (boardNum == "4") {
+        footerTitle = "여플소식";
+    } else if (boardNum == "5") {
+        footerTitle = "뉴스레터";
+    }
 
 
     return (
         <div>
-            <Header headerTitle={headerInfo} urlItems={urlData} footerTitle={'세종시 단체 및 소식'}/>
+            <Header headerTitle={headerInfo} urlItems={urlData} footerTitle={footerTitle}/>
             <ImgPostList boardNum={boardNum} />
             <Footer />
         </div>
